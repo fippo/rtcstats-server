3ae65bd6ad40e360f5ee9b240cd5d1c40d2a6e1e
Philipp Hancke <philipp@tokbox.com>
2016-01-28 15:13:14 -0800

    checks mediaType, add more features

--- features.js
+++ features.js
@@ -979,14 +979,6 @@ module.exports = {
         return extractLastVideoStat(peerConnectionLog, 'nackCount');
     },
 
-    // googMinPlayoutDelayMs -- may be used to detect desync between audio and video
-    //          Minimum playout delay (used for lip-sync). This is the minimum delay required
-    //          to sync with audio. Not included in  VideoCodingModule::Delay()
-    //          Defaults to 0 ms.
-    maxGoogMinPlayoutDelayMs: function(client, peerConnectionLog) {
-        return extractMaxVideoStat(peerConnectionLog, 'googMinPlayoutDelayMs');
-    },
-
     // TODO: jitter
     // TODO: packets lost (audio and video separated)
     // TODO: packets sent
@@ -1012,6 +1004,24 @@ module.exports = {
     };
     module.exports['mode' + stat[0].toUpperCase() + stat.substr(1)] = function(client, peerConnectionLog) {
         return extractMostCommonVideoStat(peerConnectionLog, stat);
+    };
+});
+
+// googMinPlayoutDelayMs -- may be used to detect desync between audio and video
+//          Minimum playout delay (used for lip-sync). This is the minimum delay required
+//          to sync with audio. Not included in  VideoCodingModule::Delay()
+//          Defaults to 0 ms.
+// for these stats, mode does not make sense
+['googCurrentDelayMs', 'googJitterBufferMs', 'googMinPlayoutDelayMs', 'googTargetDelayMs'].forEach(function(stat) {
+    module.exports['max' + stat[0].toUpperCase() + stat.substr(1)] = function(client, peerConnectionLog) {
+        return extractMaxVideoStat(peerConnectionLog, stat);
+    };
+    // min seem to be 0?
+    module.exports['min' + stat[0].toUpperCase() + stat.substr(1)] = function(client, peerConnectionLog) {
+        return extractMinVideoStat(peerConnectionLog, stat);
+    };
+    module.exports['mean' + stat[0].toUpperCase() + stat.substr(1)] = function(client, peerConnectionLog) {
+        return extractMeanVideoStat(peerConnectionLog, stat);
     };
 });
 
