6dd294c7e6bc1df2332f787d2d96061601c0655a
Gustavo Garcia <gustavogb@gmail.com>
2016-09-19 10:01:20 +0200

    ADD firehose support

--- extract.js
+++ extract.js
@@ -2,12 +2,14 @@ var fs = require('fs');
 var config = require('config');
 
 var Store = require('./store')({
-  s3: config.get('s3')
+  s3: config.get('s3'),
 });
 var Database = require('./database')({
-  dynamodb: config.get('dynamodb')
+  dynamodb: config.get('dynamodb'),
+  firehose: config.get('firehose'),
 });
-var isProduction = process.env.NODE_ENV && process.env.NODE_ENV === 'production';
+
+var isProduction = true;// process.env.NODE_ENV && process.env.NODE_ENV === 'production';
 
 function capitalize(str) {
     return str[0].toUpperCase() + str.substr(1);
