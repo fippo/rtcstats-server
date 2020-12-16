eb2121d1557945d5c9ade6411b5eb1a627f2f3f7
Andrei Gavrilescu <51706180+andrei-gavrilescu@users.noreply.github.com>
2020-12-16 12:27:52 +0200

    fix(features): Only use realtime as a video source (#14)
    * only use realtime as a video res source
    
    * remove deprecated comment

--- src/utils/stats-detection.js
+++ src/utils/stats-detection.js
@@ -283,9 +283,7 @@ function getUsedResolutionStandard(report, statsEntry) {
  * @return {Number} Used send resolution.
  */
 function getUsedResolutionLegacy(report) {
-    // packetsLost is a cumulative stats thus we just overwrite the value so we don't have to find
-    // the last type of stats of a certain type.
-    if (isLegacyVideoSsrcReport(report)) {
+    if (isLegacyVideoSsrcReport(report) && report.googContentType === 'realtime') {
         return extractValidResolution(report.frameHeight);
     }
 }
