7068a7649a743d56cc81cc278e6a24be931dc6b0
George Politis <gp@jitsi.org>
2022-01-25 17:51:01 +0000

    Adds Jest tests for the existing stats dumps that we have. (#34)
    * Git ignore IntelliJ project/module files.
    
    * Adds jest tests for the existing stats dumps.
    
    * Update package.json (use jest tests for testing)
    
    * fixes the tests
    
    * removes obsolete github action

--- src/test/jest/extract.test.js
+++ src/test/jest/extract.test.js
@@ -4,45 +4,75 @@ const sizeof = require('object-sizeof');
 const FeatureExtractor = require('../../features/FeatureExtractor');
 const logger = require('../../logging');
 const { StatsFormat } = require('../../utils/stats-detection');
+const { strict: assert } = require('assert');
+const fs = require('fs');
 
+async function simulateConnection(dumpPath, resultPath, statsFormat) {
+
+    const dumpMeta = {
+        endpointId: '1a404b1b',
+        dumpPath: dumpPath,
+        statsFormat: statsFormat
+    };
+
+    const featExtractor = new FeatureExtractor(dumpMeta);
+    const results = await featExtractor.extract();
+
+    const resultString = fs.readFileSync(resultPath);
+    const resultList = JSON.parse(resultString);
+    const resultTemplate = resultList.shift();
+
+    assert.deepStrictEqual(results, resultTemplate.features);
+}
 
 describe('Feature extraction tests', () => {
-    beforeEach(() => {
-        // Nothing yet
-    });
 
-    test('SFU dump feature extraction', async () => {
-        const dumpMeta = {
-            endpointId: '1a404b1b',
-            dumpPath: './src/test/jest/sfu',
-            statsFormat: StatsFormat.CHROME_STANDARD
-        };
-
-        const featExtractor = new FeatureExtractor(dumpMeta);
-
-        const results = await featExtractor.extract();
-
-        logger.info('%o', results);
-
-        expect(3).toBe(3);
+    test.skip('SFU dump feature extraction', async () => {
+        await simulateConnection(
+            './src/test/jest/sfu',
+            '',
+            StatsFormat.STANDARD
+        );
     });
 
     test.skip('SFU and P2P dump feature extraction', async () => {
-        const dumpMeta = {
-            endpointId: '1a404b1b',
-            dumpPath: './src/test/jest/sfu-p2p'
-        };
-        const featExtractor = new FeatureExtractor(dumpMeta);
+        await simulateConnection(
+            './src/test/jest/sfu-p2p',
+            '',
+            StatsFormat.STANDARD
+        );
+    });
 
-        try {
-            const results = await featExtractor.extract();
+    test('Chrome in a peer-to-peer call', async () => {
+        await simulateConnection(
+            './src/test/dumps/google-standard-stats-p2p',
+            './src/test/jest/results/google-standard-stats-p2p-result.json',
+            StatsFormat.CHROME_STANDARD
+        );
+    });
 
-        } catch (e) {
-            logger.error('Error: %o', e);
-        }
+    test('Chrome in a multi-party call', async () => {
+        await simulateConnection(
+            './src/test/dumps/google-standard-stats-sfu',
+            './src/test/jest/results/google-standard-stats-sfu-result.json',
+            StatsFormat.CHROME_STANDARD
+        );
+    });
 
+    test('Firefox in a multi-party call', async () => {
+        await simulateConnection(
+            './src/test/dumps/firefox-standard-stats-sfu',
+            './src/test/jest/results/firefox-standard-stats-sfu-result.json',
+            StatsFormat.FIREFOX
+        );
+    });
 
-        expect(3).toBe(3);
+    test('Safari in a peer-to-peer call', async () => {
+        await simulateConnection(
+            './src/test/dumps/safari-standard-stats',
+            './src/test/jest/results/safari-standard-stats-result.json',
+            StatsFormat.SAFARI
+        );
     });
 });
 
