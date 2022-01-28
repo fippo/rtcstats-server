faeb5a270655cc04281bd0fdf1d920899651dcb1
Andrei Gavrilescu <51706180+andrei-gavrilescu@users.noreply.github.com>
2022-01-28 09:35:17 +0200

    feat: send pc metrics to firehose (#38)
    * send pc metrics to firehose
    
    * version bump

--- src/database/FirehoseConnector.js
+++ src/database/FirehoseConnector.js
@@ -10,14 +10,14 @@ const { getSQLTimestamp } = require('../utils/utils');
  * Firehose will send data to s3 and then issue a COPY command to redshift.
  */
 class FirehoseConnector {
-
     /**
      *
      * @param {*} param0
      */
-    constructor({ region, stream, appEnv }) {
+    constructor({ region, meetingStatsStream, pcStatsStream, appEnv }) {
         this._awsRegion = region;
-        this._awsFirehoseStream = stream;
+        this._meetingStatsStream = meetingStatsStream;
+        this._pcStatsStream = pcStatsStream;
         this._appEnv = appEnv;
     }
 
@@ -32,48 +32,111 @@ class FirehoseConnector {
         logger.info('[Firehose] Successfully connected.');
     };
 
+    _putRecord = (schemaObj, stream) => {
+        this._firehose.putRecord(
+            {
+                DeliveryStreamName: stream /* required */,
+                Record: {
+                    Data: JSON.stringify(schemaObj)
+                }
+            },
+            err => {
+                if (err) {
+                    logger.error('[Firehose] Error sending data to firehose: %o', err);
+                    PromCollector.firehoseErrorCount.inc();
+
+                    return;
+                }
+                logger.info('[Firehose] Sent data: %o', schemaObj);
+            }
+        );
+    };
+
     /**
      * Send data to the firehose stream.
      * @param {*} param0
      */
     put = ({ dumpInfo, features }) => {
+        const {
+            clientId: statsSessionId,
+            userId: displayName,
+            conferenceId: meetingName,
+            conferenceUrl: meetingUrl,
+            sessionId: meetingUniqueId,
+            endpointId
+        } = dumpInfo;
 
-        const { sentiment } = features;
+        const {
+            aggregates = {},
+            metrics: { sessionDurationMs: sessionDuration },
+            dominantSpeakerChanges,
+            speakerTime,
+            sentiment: {
+                angry: sentimentAngry,
+                disgusted: sentimentDisgusted,
+                fearful: sentimentFearful,
+                happy: sentimentHappy,
+                neutral: sentimentNeutral,
+                sad: sentimentSad,
+                surprised: sentimentSurprised
+            }
+        } = features;
 
         // The schemaObj needs to match the redshift table schema.
-        const schemaObj = { appEnv: this._appEnv,
-            statsSessionId: dumpInfo.clientId,
-            displayName: dumpInfo.userId,
+        const schemaObj = {
+            appEnv: this._appEnv,
             createDate: getSQLTimestamp(),
-            meetingName: dumpInfo.conferenceId,
-            meetingUrl: dumpInfo.conferenceUrl,
-            meetingUniqueId: dumpInfo.sessionId,
-            endpointId: dumpInfo.endpointId,
-            sessionDuration: features.metrics.sessionDurationMs,
-            dominantSpeakerChanges: features.dominantSpeakerChanges,
-            speakerTime: features.speakerTime,
-            sentimentAngry: sentiment.angry,
-            sentimentDisgusted: sentiment.disgusted,
-            sentimentFearful: sentiment.fearful,
-            sentimentHappy: sentiment.happy,
-            sentimentNeutral: sentiment.neutral,
-            sentimentSad: sentiment.sad,
-            sentimentSurprised: sentiment.surprised
+            statsSessionId,
+            displayName,
+            meetingName,
+            meetingUrl,
+            meetingUniqueId,
+            endpointId,
+            sessionDuration,
+            dominantSpeakerChanges,
+            speakerTime,
+            sentimentAngry,
+            sentimentDisgusted,
+            sentimentFearful,
+            sentimentHappy,
+            sentimentNeutral,
+            sentimentSad,
+            sentimentSurprised
         };
 
-        this._firehose.putRecord({
-            DeliveryStreamName: this._awsFirehoseStream, /* required */
-            Record: {
-                Data: JSON.stringify(schemaObj)
-            }
-        }, err => {
-            if (err) {
-                logger.error('[Firehose] Error sending data to firehose: %o', err);
-                PromCollector.firehoseErrorCount.inc();
+        this._putRecord(schemaObj, this._meetingStatsStream);
 
-                return;
-            }
-            logger.info('[Firehose] Sent data: %o', schemaObj);
+        Object.keys(aggregates).forEach(pc => {
+            const {
+                dtlsErrors,
+                dtlsFailure,
+                isP2P,
+                trackAggregates: {
+                    receivedPacketsLostPct,
+                    sentPacketsLostPct,
+                    totalPacketsReceived,
+                    totalPacketsSent,
+                    totalReceivedPacketsLost,
+                    totalSentPacketsLost
+                },
+                transportAggregates: { meanRtt }
+            } = aggregates[pc];
+
+            const aggregateSchemaObj = {
+                statsSessionId,
+                dtlsErrors,
+                dtlsFailure,
+                isP2P,
+                receivedPacketsLostPct,
+                sentPacketsLostPct,
+                totalPacketsReceived,
+                totalPacketsSent,
+                totalReceivedPacketsLost,
+                totalSentPacketsLost,
+                meanRtt
+            };
+
+            this._putRecord(aggregateSchemaObj, this._pcStatsStream);
         });
     };
 }
