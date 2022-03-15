493d3799a891ee417612cbdf4d0f1af0d41d70dc
George Politis <georgios.politis@8x8.com>
2022-03-15 13:55:53 +0000

    Updates deploy.sh (#53)

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -21,6 +21,8 @@ if [[ -z "$RTCSTATS_REPOSITORY" ]]; then
   exit 1
 fi
 
+ACCOUNT_ID=$(echo $RTCSTATS_REPOSITORY | cut -d. -f1)
+REGION=$(echo $RTCSTATS_REPOSITORY | cut -d. -f4)
 REPOSITORY=${RTCSTATS_REPOSITORY}:rtcstats-server-${VERSION}
 
 docker build -t $REPOSITORY .
@@ -28,6 +30,15 @@ docker build -t $REPOSITORY .
 if [[ ${SHOULD_PUSH_IMAGE} == true ]]
   then
     echo "push docker image to ecr"
-    $(aws ecr get-login --region us-west-2 --no-include-email)
+    # aws --version
+    # aws-cli/2.2.30 Python/3.8.8 Linux/5.10.16.3-microsoft-standard-WSL2 exe/x86_64.ubuntu.20 prompt/off
+    if [ $(aws --version|cut -d' ' -f1|cut -d/ -f2 | cut -d. -f1) -gt 1 ]
+    then
+      aws ecr get-login-password --region $REGION | docker login \
+          --username AWS \
+          --password-stdin $ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com
+    else
+        $(aws ecr get-login --region $REGION --no-include-email)
+    fi
     docker push $REPOSITORY
 fi
